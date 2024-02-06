496c40662f1315dc8227032feec995be91a9d1b7
SameepRungta <sameeprungta2002@gmail.com>
2024-02-06 16:28:53 +0530

    module 11

---
+++ destini/lib/story.dart
@@ -0,0 +1,12 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  // Constructor with parameters
+  Story({
+    required this.storyTitle,
+    required this.choice1,
+    required this.choice2,
+  });
+}
